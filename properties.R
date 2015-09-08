30becd80060a434b5a6e6e84c4771635b3980e66
tanovsky <tanovsky@gmail.com>
2015-09-08 17:04:14 +0200

    Update properties.R

--- properties.R
+++ properties.R
@@ -1,12 +1,11 @@
 # Use full paths only..'~' not working..
 dataRoot <- "/data/CNVPanelizer/ION_TORRENT_DATA"
-outputDirectory <- "/wip/scripts/CNVPanelExomComparison/CNVPanelizer/output"
-referenceDirectory <- file.path(dataRoot, "HIPOReference")
-sampleDirectory <- file.path(dataRoot, "HIPO21")
+outputDirectory <- "/output"
+referenceDirectory <- file.path(dataRoot, "reference")
+sampleDirectory <- file.path(dataRoot, "sample")
 bedFilepath <- file.path(dataRoot, "ACPv2_WITHOUT_CHR.bed")
 ampliconColumnNumber <- 6
 removePcrDuplicates <- TRUE
 numberOfBootstrapReplicates <- 10000
 plotsOutputDirectory <- file.path(outputDirectory, "plots")
 tablesOutputDirectory <- file.path(outputDirectory, "tables")
-
